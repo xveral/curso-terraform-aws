9175b37d46be3b8b93b5070375a56b8ce4bd133e
Javier Veral <javier.veral@adevinta.com>
2026-01-19 12:56:59 +0100

    Style: Aplicando formato automático terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "url_web" {
-    description = "URL con IP elastica"  
-    value = "http://${aws_eip.ip_fija.public_ip}"
+  description = "URL con IP elastica"
+  value       = "http://${aws_eip.ip_fija.public_ip}"
 }
 
 output "comando_ssh" {
-    description = "SSH con IP elastica"
-    value = "ssh ec2-user@${aws_eip.ip_fija.public_ip}"
+  description = "SSH con IP elastica"
+  value       = "ssh ec2-user@${aws_eip.ip_fija.public_ip}"
 }
 
 output "ip_privada_fija" {
-    value = aws_instance.web.private_ip
+  value = aws_instance.web.private_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "region_aws" {
-    description = "La region donde se despliega"
-    type = string
-    default = "eu-south-2"
+  description = "La region donde se despliega"
+  type        = string
+  default     = "eu-south-2"
 }
 
 variable "tipo_instancia" {
-    description = "El tamaño del server"
-    type = string
-    default = "t3.micro"
+  description = "El tamaño del server"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "puerto_web" {
-    description = "Puerto para server web"
-    type = number
-    default = 80
+  description = "Puerto para server web"
+  type        = number
+  default     = 80
 }
